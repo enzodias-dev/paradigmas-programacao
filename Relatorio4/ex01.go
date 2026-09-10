d5f709be6306d541e801695dc7bfb341f64bf13d
Enzo Dias <155120152+enzodias-dev@users.noreply.github.com>
2026-09-10 17:36:22 -0300

    Add files via upload

--- Relatorio4/ex01.go
+++ Relatorio4/ex01.go
@@ -1,1 +1,29 @@
+package main
 
+import "fmt"
+
+func ValidarCodigoRastreio(codigo string) (bool, string) {
+	if len(codigo) == 10 {
+		return true, "Código de rastreio registrado no sistema!"
+	}
+
+	return false, "Erro: O código de rastreio deve ter exatamente 10 caracteres."
+}
+
+func main() {
+	var codigo string
+
+	for {
+		fmt.Print("Digite o código de rastreio: ")
+		fmt.Scanln(&codigo)
+
+		valido, mensagem := ValidarCodigoRastreio(codigo)
+
+		if valido {
+			fmt.Println(mensagem)
+			break
+		}
+
+		fmt.Println(mensagem)
+	}
+}
